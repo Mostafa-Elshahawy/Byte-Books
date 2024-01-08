5a27ef796c0bc921db53d57b530da191c2668c05
Mostafa Elshahawy <mostafaelshahawy67@gmail.com>
2024-01-08 21:56:46 +0200

    added database driver and .env vars

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,28 @@
 package main
 
 import (
+	"log"
+	"os"
+
+	"github.com/ME/Byte-Books/internal/database"
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 )
 
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
 func main() {
+	DB, err := database.ConnectSQL(os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer DB.SQL.Close()
+
 	mux := echo.New()
 	Routers(mux)
 	mux.Logger.Fatal(mux.Start(":8000"))
