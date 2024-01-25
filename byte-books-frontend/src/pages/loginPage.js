061435f7b0c817e01aae4833471a0c3647aec0cc
mostafaba29 <44523064+mostafaba29@users.noreply.github.com>
2024-01-25 21:03:17 +0200

    initialized react app and added basic compontents for login and signup

---
+++ byte-books-frontend/src/pages/loginPage.js
@@ -0,0 +1,51 @@
+import React,{useState} from 'react';
+import axios from 'axios';
+import Navbar from '../components/navbar';
+import Form from '../components/Form';
+import Footer from '../components/Footer';
+
+const LoginPage = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = async ()=>{
+        try {
+            const response = await axios.post('http://localhost:5000/login', {
+                username,
+                password,
+            });
+            console.log('loginSuccessful', response.data);
+        }catch (error) {
+            console.error('error during login',error.response.data);
+        }
+    };
+    return (
+        <div>
+        <Navbar />
+        <Form
+          title="Login"
+          onSubmit={handleLogin}
+        >
+          <label>
+            Username:
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </label>
+          <label>
+            Password:
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+        </Form>
+        <Footer />
+      </div>
+    );
+};
+
+export default LoginPage;
---
+++ byte-books-frontend/src/pages/signupPage.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import Navbar from '../components/navbar';
+import Form from '../components/Form';
+import Footer from '../components/Footer';
+
+const SignupPage = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const handleSignup = async ()=>{
+        try {
+            const response = await axios.post('http://localhost:5000/signup', {
+              username,
+              password,
+            });
+      
+            // Handle successful signup response
+            console.log('Signup successful:', response.data);
+          } catch (error) {
+            // Handle signup error
+            console.error('Signup error:', error.response.data);
+          }
+    };
+    return (
+        <div>
+        <Navbar />
+        <Form
+          title="Signup"
+          onSubmit={handleSignup}
+        >
+          <label>
+            Username:
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </label>
+          <label>
+            Password:
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+        </Form>
+        <Footer />
+      </div>
+    );
+};
+
+export default SignupPage;
