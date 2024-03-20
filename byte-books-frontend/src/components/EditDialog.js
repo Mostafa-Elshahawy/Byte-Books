a34094e528e878e9d870284279f7695ebdb9aa5a
mostafaba29 <44523064+mostafaba29@users.noreply.github.com>
2024-03-20 14:38:40 +0200

    added some feateures

--- byte-books-frontend/src/components/EditDialog.js
+++ byte-books-frontend/src/components/EditDialog.js
@@ -1,18 +1,56 @@
 import React from 'react';
 import { Dialog, DialogTitle, DialogContent, DialogActions, Button, TextField } from '@mui/material';
 
-const EditDialog = ({open,handleClose,Product,handleSave,handleEdit}) => {
+const EditDialog = ({ open, handleClose, Product, handleSave, handleEdit }) => {
+    const handleChange = (field, value) => {
+        let parsedValue;
 
-    
-    return(
+        // Check if the field is 'price' or 'quantity' and parse accordingly
+        if (field === 'price') {
+            parsedValue = parseFloat(value);
+        } else if (field === 'quantity') {
+            parsedValue = parseInt(value, 10); // The second argument, 10, specifies the radix (base) for the conversion
+        } else {
+            parsedValue = value;
+        }
+
+        handleEdit({ ...Product, [field]: parsedValue });
+    };
+
+    return (
         <Dialog open={open} onClose={handleClose}>
             <DialogTitle>Edit Product</DialogTitle>
             <DialogContent>
-                <TextField margin='dense' label='name' value={Product?.name || ''} onChange={(e)=>handleEdit({...Product,name:e.target.value})}/>
-                <TextField margin='dense'label='description' value={Product?.description || ''} onChange={(e)=>handleEdit({...Product,description:e.target.value})}/>
-                <TextField margin='dense'label='author' value={Product?.author || ''} onChange={(e)=>handleEdit({...Product,author:e.target.value})}/>
-                <TextField margin='dense'label='price' value={Product?.price || ''} onChange={(e)=>handleEdit({...Product,price:e.target.value})}/>
-                <TextField margin='dense'label='quantity' value={Product?.quantity || ''} onChange={(e)=>handleEdit({...Product,quantity:e.target.value})}/>
+                <TextField
+                    margin='dense'
+                    label='name'
+                    value={Product?.name || ''}
+                    onChange={(e) => handleChange('name', e.target.value)}
+                />
+                <TextField
+                    margin='dense'
+                    label='description'
+                    value={Product?.description || ''}
+                    onChange={(e) => handleChange('description', e.target.value)}
+                />
+                <TextField
+                    margin='dense'
+                    label='author'
+                    value={Product?.author || ''}
+                    onChange={(e) => handleChange('author', e.target.value)}
+                />
+                <TextField
+                    margin='dense'
+                    label='price'
+                    value={Product?.price || ''}
+                    onChange={(e) => handleChange('price', e.target.value)}
+                />
+                <TextField
+                    margin='dense'
+                    label='quantity'
+                    value={Product?.quantity || ''}
+                    onChange={(e) => handleChange('quantity', e.target.value)}
+                />
             </DialogContent>
             <DialogActions>
                 <Button color='secondary' onClick={handleClose}>Cancel</Button>
--- byte-books-frontend/src/components/Product.js
+++ byte-books-frontend/src/components/Product.js
@@ -1,72 +1,82 @@
 import React from 'react';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { useHistory ,Link} from 'react-router-dom';
+import { useHistory, Link } from 'react-router-dom';
 import Card from '@mui/material/Card';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import { Button } from '@mui/material';
-//import StarIcon from '@mui/icons-material/Star';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
-const theme = createTheme   ({
-    palette: {
-        primary: {
-            main: '#944e63',
-        },
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#944e63',
     },
-    overrides: {
-        MuiCard:{
-            root:{
-                width:'100',
-                height:'120px',
-                marign:'8px',
-                borderRadius:'8px',
-                boxShadow:'0px 4px 6px rgba(0, 0, 0, 0.)',
-                transition: 'box-shadow .3 ease-in-out',
-                '&:hover': {
-                    boxshadow:'0px 8px 12px rgba(0, 0, 0, 0.2)',
-                },
-            },
-        },
-    },
-    MuiTypography: {
-        subtitle1: {
-          color: '#333',
-        },
-        h4: {
-          color: '#f00',
+  },
+  overrides: {
+    MuiCard: {
+      root: {
+        width: '300px',
+        height: '400px',
+        margin: '8px',
+        borderRadius: '8px',
+        boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.)',
+        transition: 'box-shadow .3s ease-in-out',
+        '&:hover': {
+          boxShadow: '0px 8px 12px rgba(0, 0, 0, 0.2)',
         },
       },
-    });
+    },
+  },
+  MuiTypography: {
+    subtitle1: {
+      color: '#333',
+    },
+    h4: {
+      color: '#f00',
+    },
+  },
+});
 
-    const Product= ({bookName,Author,imageSrc,Price,ProductID})=> {
-        const history = useHistory();
+const Product = ({ bookName, Author, imageSrc, Price, ProductID }) => {
+  const history = useHistory();
 
-        const handleClick = ()=>{
-            history.push(`/product/${ProductID}`);
-        };
-        
-        return (
-            <ThemeProvider theme={theme}>
-                <Card onClick={handleClick}>
-                    <CardMedia component="img" src={`${process.env.PUBLIC_URL}/images/${imageSrc}`} alt={`${bookName}`} style={{ maxWidth: 250, maxHeight: 320 }}/>
-                    <CardContent>
-                        <Typography variant ='h5'>{bookName}</Typography>
-                        <Typography variant ='subtitle1'>{Author}</Typography>
-                        {/*<div className='starRating'>
-                            <StarIcon />
-                            <StarIcon />
-                            <StarIcon />
-                            <StarIcon />
-                            <StarIcon />
-                        </div>*/}
-                        <Typography variant='h6'>{Price}$</Typography>
-                    </CardContent>
-                    <Button component={Link} to='../pages/Cart' color='inherit'><ShoppingCartIcon className='cart' /></Button>
-                </Card>
-            </ThemeProvider>
-        );
-    };
+  const handleClick = () => {
+    history.push(`/product/${ProductID}`);
+  };
 
-    export default Product;
+  return (
+    <ThemeProvider theme={theme}>
+      <Card onClick={handleClick}>
+        <div style={{ position: 'relative', height: '425px' }}>
+          <CardMedia
+            component="img"
+            src={`${process.env.PUBLIC_URL}/images/${imageSrc}`}
+            alt={`${bookName}`}
+            style={{
+              width: '100%',
+              height: '100%',
+              objectFit: 'cover',
+              position: 'absolute',
+              top: 0,
+              left: 0,
+            }}
+          />
+        </div>
+        <CardContent style={{ textAlign: 'center' }}>
+          <Typography variant="h5">{bookName}</Typography>
+          <Typography variant="subtitle1" style={{ marginBottom: '10px' }}>
+            {Author}
+          </Typography>
+          <Typography variant="h6">{Price}$</Typography>
+        </CardContent>
+        <Button component={Link} to="../pages/Cart" color="inherit">
+          <ShoppingCartIcon className="cart" />
+        </Button>
+      </Card>
+    </ThemeProvider>
+  );
+};
+
+export default Product;
